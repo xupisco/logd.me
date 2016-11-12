02b68c2ec9d4385c32084187c1a9cf855128fec7
Leandro Voltolino <xupisco@gmail.com>
2016-11-12 14:09:55 -0200

    welcome post

---
+++ apps/logs/default_log.py
@@ -0,0 +1,28 @@
+def log_en():
+    body = """
+        <p><p><strong>Hi there, welcome to ownCRM!</strong></p>
+        <p>This is your <span class="hashtag">#personal</span> life log, where you can save things that happened or will happen, register important events, meetings, phone calls, anything... so you can easily remind whenever you need.</p>
+        <p>We built this app to be simple and fast, these are some core features:</p>
+        <ul>
+        <li>Rich text editor</li>
+        <li>@mentions for people and companies</li>
+        <li>Tags</li>
+        <li>Keyboard shortcuts</li>
+        <li>Extremily easy and fast fuzzy search by:</li>
+        <ul>
+        <li>Contents</li>
+        <li>Tags</li>
+        <li>Dates</li>
+        <li>Highlights (search for "!!!")</li>
+        <li>@mentions</li>
+        <li>Tags <span class="hashtag">#welcome</span> <span class="hashtag">#owncrm</span></li>
+        </ul>
+        </ul>
+        <p>Here are some sample people, like <span class="hl_mention_person" data-id="3">John Doe</span>&nbsp;who is nobody and <span class="hl_mention_person" data-id="5">Gill Bates</span>&nbsp;who works at <span class="hl_mention_company" data-id="3">ACME Corporation</span>, all in a default <span class="hashtag">#inbox</span> category for you to get started.</p>
+        <p>You can delete those guys later.<br>Hope you like it!</p>
+        <blockquote>
+        <p><strong>Quick <span class="hashtag">#tips</span></strong></p>
+        <p>Type '@', in editor, to bring up mentions suggestions<br>Type '#', in editor, to add a Tag<br>Type '/' to focus at search input<br>Type 'esc' to clear search<br>Search for '!!!' to filter highlighted logs</p>
+        </blockquote>
+        <p>Regards<strong><br></strong><strong>ownCRM Team</strong></p></p>
+    """
