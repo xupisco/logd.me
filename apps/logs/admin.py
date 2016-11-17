dca714d3bca5bf856cba53f952757048ae2d2e1c
Leandro Voltolino <xupisco@gmail.com>
2016-11-17 14:50:47 -0200

    admin tweaks

--- apps/logs/admin.py
+++ apps/logs/admin.py
@@ -1,17 +1,39 @@
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
+from django.utils.html import strip_tags
 
 from .models import LogKind, Log
 
 
 class LogKindAdmin(admin.ModelAdmin):
+    list_display = ('owner_name', 'name', 'c_usage', )
     prepopulated_fields = {'slug': ('name',)}
+
+    def owner_name(self, obj):
+        return obj.owner.get_full_name()
+    owner_name.short_description = _('owner')
+
+    def c_usage(self, obj):
+        return obj.log_set.count()
+    c_usage.short_description = _('usage')
 
 
 class LogAdmin(admin.ModelAdmin):
-    list_display = ('kind', 'reminder', 'start_date', 'c_companies', 'c_people', )
+    list_display = ('owner_name', 'kind', 'short_body', 'reminder', 'start_date', 'c_companies', 'c_people', )
     list_filter = ('reminder', )
+    list_display_links = ('kind', )
     search_fields = ('body', 'companies__name', 'people__name', )
+
+    def owner_name(self, obj):
+        return obj.owner.get_full_name()
+    owner_name.short_description = _('owner')
+
+    def short_body(self, obj):
+        body = strip_tags(obj.body)
+        ml = 50
+        return (body[:ml] + '...') if len(body) > ml else body
+    short_body.allow_tags = True
+    short_body.short_description = _('preview')
 
     def c_companies(self, obj):
         return obj.companies.count()
--- apps/people/admin.py
+++ apps/people/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from .models import Role, Person
 
@@ -8,9 +9,14 @@ class RoleAdmin(admin.ModelAdmin):
 
 
 class PersonAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'role', 'mobile', 'company', )
+    list_display = ('owner_name', 'name', 'email', 'role', 'mobile', 'company', )
     list_filter = ('role', 'company', )
     search_fields = ('name', 'role__name', 'company__name', )
+    list_display_links = ('name', )
+
+    def owner_name(self, obj):
+        return obj.owner.get_full_name()
+    owner_name.short_description = _('owner')
 
 
 admin.site.register(Role, RoleAdmin)
