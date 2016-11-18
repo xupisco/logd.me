4cd6a69e36919b2471cf30d63ffd3818e41dec80
Leandro Voltolino <xupisco@gmail.com>
2016-11-18 15:26:15 -0200

    fixed?

--- apps/core/views.py
+++ apps/core/views.py
@@ -48,11 +48,12 @@ def public(request, encoded):
     hashids = Hashids(salt=settings.SECRET_KEY)
     decoded = hashids.decode(encoded)
 
-    try:
-        log = Log.objects.get(id=decoded[1], owner=decoded[0])
-        return render(request, 'public.html', {'public': True, 'log': log})
-    except:
-        return redirect('/')
+    log = Log.objects.get(id=decoded[1], owner=decoded[0])
+    #return render(request, 'public.html', {'public': True, 'log': log})
+
+    response_data = {}
+    response_data['salame'] = decoded
+    response_data['encoded'] = encoded
 
     return HttpResponse(json.dumps(response_data), content_type="application/json")
 
