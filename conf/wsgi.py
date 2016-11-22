c40d8de984cca3d4d53fb0aa94734a5e8bec7ff6
Leandro Voltolino <xupisco@gmail.com>
2016-11-22 17:20:02 -0200

    added newlrelic

--- conf/wsgi.py
+++ conf/wsgi.py
@@ -8,11 +8,16 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import settings
+import newrelic.agent
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+
+newrelic.agent.initialize(os.path.join(BASE_DIR, 'conf') + '/newrelic.ini')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
+application = newrelic.agent.WSGIApplicationWrapper(application)
